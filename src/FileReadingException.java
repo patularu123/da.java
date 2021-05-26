2c95d90aae3c68443fdbc455e6d0a22f8769950f
patularu123 <61266127+patularu123@users.noreply.github.com>
2021-05-26 18:14:36 +0300

    prj

---
+++ src/FileReadingException.java
@@ -0,0 +1,10 @@
+public class FileReadingException extends Exception {
+
+    public FileReadingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public FileReadingException(String message) {
+        super(message);
+    }
+}
---
+++ src/FileWritingException.java
@@ -0,0 +1,10 @@
+public class FileWritingException extends Exception {
+
+    public FileWritingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public FileWritingException(String message) {
+        super(message);
+    }
+}
